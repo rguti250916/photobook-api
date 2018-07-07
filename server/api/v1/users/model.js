b8c3e2be4c4cfddc69ef0b62c3f7a11ca189cbd3
Rafael Gutierrez <ing.rafael.gutierrez.correales@gmail.com>
2018-07-07 14:00:06 -0500

    Model User changed.

--- server/api/v1/users/model.js
+++ server/api/v1/users/model.js
@@ -5,27 +5,45 @@ const {
 } = mongoose;
 
 const fields = {
+  firstname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
   photo_url: {
     type: String,
     required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    min: 6,
   },
   description: {
     type: String,
     default: '',
     trim: true,
   },
-  location: {
-    type: String,
-    default: '',
-    trim: true,
-  },
 };
 
-const post = new Schema(fields, {
+const user = new Schema(fields, {
   timestamps: true,
 });
 
 module.exports = {
-  Model: mongoose.model('post', post),
+  Model: mongoose.model('user', user),
   fields,
 };
